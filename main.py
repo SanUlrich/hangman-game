4239a7e4ec475f4d4093800c22a6e224ebd67666
SanUlrich <SanUlrich@mail.ru>
2023-05-01 07:36:08 +0200

    add docstrings and comments

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ from words import WORDS
 
 
 def get_word(words: list):
+    """
+    Select random word from list
+    :param words: list of words
+    :return: random word from list
+    """
     secret_word = random.choice(words)
     return secret_word.lower()
 
 
-def check_guess(letter):
+def check_guess(letter: str):
+    """
+    Check that input is just one letter
+    :param letter: input string
+    :return: one letter (str) in lowercase
+    """
     if len(letter) != 1:
         print('Введите ОДНУ букву.')
     elif not letter.isalpha():
@@ -19,25 +29,35 @@ def check_guess(letter):
 
 
 def retry():
+    """
+    Ask user for retry.
+    :return: call start_game() with random word if answer is 'yes'
+    """
     ans = input('Сыграть еще? (д/н): ')
     if ans.lower() in ['д', 'да', 'y', 'yes', 'da']:
         start_game(get_word(WORDS))
 
 
-def start_game(word):
-    word_form = list('_' * len(word))
-    secret_word = list(word)
-    guessed_letters = []
-    out_of_tries = False
-    fails = 0
+def start_game(word: str):
+    """
+    Main game loop. Wile game is on: wait for user input (some letter).
+    :param word: random word (str)
+    :return: win/loss. Ask for retry
+    """
+
+    word_form = list('_' * len(word))   # Make field for input letters like " _____ "
+    secret_word = list(word)    # Convert secret word to list for ease of comparison
+    guessed_letters = []    # List with user input letters
+    out_of_tries = False    # Loss check
+    fails = 0   # Count of failed attempts
 
     while word_form != secret_word and not out_of_tries:
         print(HANGMANPICS[fails])
         print('"', ''.join(word_form).upper(), '"')
-        guess = check_guess(input('Введите букву: '))
+        guess = check_guess(input('Введите букву: '))   # Returns one letter or None
 
         if guess is not None:
-            if guess in guessed_letters:
+            if guess in guessed_letters:    # If the entered letter has already been
                 print(f'Буква "{guess.upper()}" уже была.')
             elif guess not in set(secret_word):
                 print(f'Буквы "{guess.upper()}" в слове нет.')
@@ -52,11 +72,13 @@ def start_game(word):
                 print(f'Буква "{guess.upper()}" есть в слове.')
                 guessed_letters.append(guess)
 
+        # Setting a letter in its place in the word field
         for k, v in enumerate(secret_word):
             if v in guessed_letters:
                 word_form.pop(k)
                 word_form.insert(k, v)
 
+    # Victory check
     if not out_of_tries:
         print(HANGMANPICS[fails])
         print('"', ''.join(secret_word).upper(), '"')
@@ -64,9 +86,13 @@ def start_game(word):
 
 
 def main():
+    """
+    Launch game.
+    :return:
+    """
     print('Добро пожаловать на виселицу!')
-    secret_word = get_word(WORDS)
-    start_game(secret_word)
+    secret_word = get_word(WORDS)   # Get random word
+    start_game(secret_word) # Start
     retry()
 
 
